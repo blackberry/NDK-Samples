c842272f4a9795ac0b25a5bacd493d02ebcef314
Jonathan Hoffmann <jhoffmann@rim.com>
2012-11-28 11:51:28 -0500

    update Makefiles, project files and bar-descriptor.xml files
    - includes removal of sample splashscreens

--- CubeRotate/common.mk
+++ CubeRotate/common.mk
@@ -5,20 +5,29 @@ include $(QCONFIG)
 
 USEFILE=
 
-include $(MKFILES_ROOT)/qmacros.mk
+# Extra include path for libfreetype and for target overrides and patches
+EXTRA_INCVPATH+=$(QNX_TARGET)/usr/include/freetype2 \
+	$(QNX_TARGET)/../target-override/usr/include
 
-# Suppress the _g suffix from the debug variant
-BUILDNAME=$(IMAGE_PREF_$(BUILD_TYPE))$(NAME)$(IMAGE_SUFF_$(BUILD_TYPE))
+# Extra library search path for target overrides and patches
+EXTRA_LIBVPATH+=$(QNX_TARGET)/../target-override/$(CPUVARDIR)/lib \
+	$(QNX_TARGET)/../target-override/$(CPUVARDIR)/usr/lib
 
-# Compiler options for enhanced security
-CCFLAGS+=-fstack-protector-all -D_FORTIFY_SOURCE=2 \
-	$(if $(filter g so shared,$(VARIANTS)),,-fPIE)
+# Compiler options for enhanced security and recording the compiler options in release builds
+CCFLAGS+=-fstack-protector-strong -D_FORTIFY_SOURCE=2 \
+	$(if $(filter g so shared,$(VARIANTS)),,-fPIE) \
+	$(if $(filter g,$(VARIANTS)),,-frecord-gcc-switches)
 
 # Linker options for enhanced security
 LDFLAGS+=-Wl,-z,relro -Wl,-z,now $(if $(filter g so shared,$(VARIANTS)),,-pie)
 
-# Basic libraries required by most native applications
+# Add your required library names, here
 LIBS+=glview GLESv1_CM m
+
+include $(MKFILES_ROOT)/qmacros.mk
+
+# Suppress the _g suffix from the debug variant
+BUILDNAME=$(IMAGE_PREF_$(BUILD_TYPE))$(NAME)$(IMAGE_SUFF_$(BUILD_TYPE))
 
 include $(MKFILES_ROOT)/qtargets.mk
 
