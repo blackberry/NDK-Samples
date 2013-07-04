de09cecb7d0d55d1c3abce3e164960e9f22d6fb4
Serge Velikevitch <svelikevitch@blackberry.com>
2013-07-04 17:38:33 -0400

    removed commented code and fixed idents

--- SmartCardGetId/main.c
+++ SmartCardGetId/main.c
@@ -1,17 +1,18 @@
 /*
- * main.c
+ * Copyright (c) 2013 Research In Motion Limited.
  *
- *  Created on: Jun 17, 2013
- *      Author: svelikevitch
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
-//#include "smartcard/sc_data_types.h"
-//#include "smartcard/sc_key_store_data.h"
-//#include "smartcard/sc_reader_state.h"
-//#include "smartcard/sc_certificate.h"
-//#include "smartcard/sc_crypto_token.h"
-//#include "smartcard/sc_crypto_system.h"
-//#include "smartcard/sc_smart_card_id.h"
 
 #include <stdlib.h>
 #include "smartcard/sc_smart_card.h"
@@ -21,8 +22,8 @@
 
 int main()
 {
-	sc_reader_name_t readername = "MediaCardSmartCardReader";
-	sc_response_code_t rc = SC_SCARD_S_SUCCESS;
+    sc_reader_name_t readername = "MediaCardSmartCardReader";
+    sc_response_code_t rc = SC_SCARD_S_SUCCESS;
 
     sc_context_t* context = 0;
 
@@ -92,9 +93,8 @@ int main()
     rc = sc_detach( context, SC_SCARD_RESET_CARD );
     CHECKRC( rc );
 
-
 end:
-	// Destroys sc_smart_card_id_t instance.
+    // Destroys sc_smart_card_id_t instance.
     sc_smart_card_id_destroy( id );
 
     // Deallocates specified handle.
